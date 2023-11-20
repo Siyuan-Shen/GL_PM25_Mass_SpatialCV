570c444584a50e341d93e3ab23f8a763deadd5f7
Shen <s.siyuan@compute1-client-1.ris.wustl.edu>
2023-11-20 17:03:42 -0600

    Update AVD Spatial CV

---
+++ Spatial_CV/iostram.py
@@ -0,0 +1,42 @@
+import csv
+import numpy as np
+from Spatial_CV.data_func import *
+
+
+def output_text(outfile:str,status:str,Areas:list,Area_beginyears:dict,endyear:int,
+                test_CV_R2, train_CV_R2, geo_CV_R2, RMSE_CV_R2, slope_CV_R2, PWAModel, PWAMonitors):
+    
+    MONTH = ['Annual','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    test_CV_R2_Alltime, train_CV_R2_Alltime, geo_CV_R2_Alltime, RMSE_CV_R2_Alltime, slope_CV_R2_Alltime, PWAModel_Alltime, PWAMonitors_Alltime = calculate_Alltime_Statistics_results(Areas,Area_beginyears,endyear,test_CV_R2, train_CV_R2, geo_CV_R2, RMSE_CV_R2, slope_CV_R2, PWAModel, PWAMonitors)
+
+    with open(outfile,status) as csvfile:
+        writer = csv.writer(csvfile)
+        for iarea in Areas:
+            writer.writerow(['Area: {} ; Time Period: {} - {}'.format(iarea, Area_beginyears[iarea], endyear)])
+        
+            for imonth in range(MONTH):
+                writer.writerow([' -------------------------- {} ------------------------'.format(imonth), 
+                            '\n Test R2 - Avg: ', str(np.round(test_CV_R2_Alltime[iarea]['Alltime'][imonth][0], 4)), 'Min: ',
+                             str(np.round(test_CV_R2_Alltime[iarea]['Alltime'][imonth][1], 4)), 'Max: ',str(np.round(test_CV_R2_Alltime[iarea]['Alltime'][imonth][2],4)),
+
+                             '\n Slope - Avg: ', str(np.round(slope_CV_R2_Alltime[iarea]['Alltime'][imonth][0], 4)), 'Min: ',
+                             str(np.round(slope_CV_R2_Alltime[iarea]['Alltime'][imonth][1], 4)), 'Max: ',str(np.round(slope_CV_R2_Alltime[iarea]['Alltime'][imonth][2],4)),
+
+                             '\n RMSE -  Avg: ', str(np.round(RMSE_CV_R2_Alltime[iarea]['Alltime'][imonth][0], 4)), 'Min: ',
+                             str(np.round(RMSE_CV_R2_Alltime[iarea]['Alltime'][imonth][1], 4)), 'Max: ',str(np.round(RMSE_CV_R2_Alltime[iarea]['Alltime'][imonth][2],4)),
+
+                             '\n Training R2 - Avg: ',str(np.round(train_CV_R2_Alltime[iarea]['Alltime'][imonth][0], 4)), 'Min: ',str(np.round(train_CV_R2_Alltime[iarea]['Alltime'][imonth][1], 4)), 'Max: ',
+                             str(np.round(train_CV_R2_Alltime[iarea]['Alltime'][imonth][2],4)),
+
+                             '\n Geophysical R2 - Avg: ',str(np.round(geo_CV_R2_Alltime[iarea]['Alltime'][imonth][0], 4)), 'Min: ',str(np.round(geo_CV_R2_Alltime[iarea]['Alltime'][imonth][1], 4)), 'Max: ',
+                             str(np.round(geo_CV_R2_Alltime[iarea]['Alltime'][imonth][2],4)), 
+                             
+                             '\n PWA Model - Avg: ',str(np.round(PWAModel_Alltime[iarea]['Alltime'][imonth][0], 4)), 'Min: ',str(np.round(PWAModel_Alltime[iarea]['Alltime'][imonth][1], 4)), 'Max: ',
+                             str(np.round(PWAModel_Alltime[iarea]['Alltime'][imonth][2],4)), 
+
+                             '\n PWA co-monitors - Avg: ',str(np.round(PWAMonitors_Alltime[iarea]['Alltime'][imonth][0], 4)), 'Min: ',str(np.round(PWAMonitors_Alltime[iarea]['Alltime'][imonth][1], 4)), 'Max: ',
+                             str(np.round(PWAMonitors_Alltime[iarea]['Alltime'][imonth][2],4)), 
+                             ])
+                
+
+    return
