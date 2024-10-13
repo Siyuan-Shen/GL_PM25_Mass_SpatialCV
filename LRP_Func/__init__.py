c9fd5e53c806a8bae7f333bf9a3c315c93ecc27b
Siyuan-Shen <s.siyuan@wustl.edu>
2024-10-12 20:03:19 -0500

    Delete LRP_Func directory

--- LRP_Func/__init__.py
+++
@@ -1,5 +0,0 @@
-##################################################################
-## This .py package is used to calculate the relevance score of ##
-## CNN based on Pytorch toolkit.                                ##
-##################################################################
-
